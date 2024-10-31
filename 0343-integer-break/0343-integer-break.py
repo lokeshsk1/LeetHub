b721d9b118664db88949909a1eade02bead00c9e
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2024-10-31 20:59:42 +0530

    Time: 0 ms (100%), Space: 16.6 MB (19.84%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(2, n+1):
             for j in range(1, min(i,4)):
-                dp[i] = max(dp[i], j*max(dp[i-j], i-j))
+                dp[i] = max(dp[i], j * max(dp[i-j], i-j))
 
         return dp[n]
 
