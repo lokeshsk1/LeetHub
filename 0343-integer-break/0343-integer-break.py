b0b82b348e88874414f543158174dded0cbffba2
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2024-10-31 20:59:23 +0530

    Time: 0 ms (100%), Space: 16.6 MB (19.84%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,9 +1,7 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
 
-        
         dp = [0 for _ in range(n+1)]
-        dp[1] = 1
 
         for i in range(2, n+1):
             for j in range(1, min(i,4)):
