c976b8a619e204b51d73b4d9e88bbf4929b70b1a
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2024-11-07 18:03:06 +0530

    Time: 464 ms (73.75%), Space: 26.6 MB (96.06%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -3,13 +3,13 @@ class Solution:
     def largestCombination(self, nums: List[int]) -> int:
 
         res = 1
-        bits = [0]*24
+        bits = [0]*24 #10^7 has 24 bits
 
         for i in range(24):
-            c = 0
+            set_bits_at_ith_position = 0
             for n in nums:
                 ith_bit = n >> i
-                c += ith_bit & 1
-            res = max(res, c)
+                set_bits_at_ith_position += ith_bit & 1
+            res = max(res, set_bits_at_ith_position)
         
         return res
