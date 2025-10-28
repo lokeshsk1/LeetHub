8e79d77c71c74e6aa4b9e49a64ee996aa8428f5e
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2025-10-29 00:57:33 +0530

    Time: 50 ms (47.34%), Space: 17.9 MB (9.04%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
@@ -7,23 +7,19 @@ class Solution:
 
         tot = sum(nums)
         tot_till = 0
+        res = 0
 
         for i in range(n):
 
             left_sum[i] = tot_till
             tot_till += nums[i]
-            
             right_sum[i] = tot - tot_till
 
-        print(left_sum, right_sum)
-
-        res = 0
-        for i in range(n):
             if nums[i] == 0:
                 if abs(left_sum[i] - right_sum[i]) == 1:
                     res += 1
                 elif left_sum[i] - right_sum[i] == 0:
-                    res += 2
+                    res += 2            
 
         return res
         
