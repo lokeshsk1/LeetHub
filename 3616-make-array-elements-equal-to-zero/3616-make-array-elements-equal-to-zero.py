b09640a6f63a1c35da2455c2a833d83dbbf0a427
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2025-10-29 01:00:02 +0530

    Time: 42 ms (90.96%), Space: 17.9 MB (9.04%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
@@ -2,8 +2,6 @@ class Solution:
     def countValidSelections(self, nums: List[int]) -> int:
         
         n = len(nums)
-        left_sum = [0]*n
-        right_sum = [0]*n
 
         tot = sum(nums)
         tot_till = 0
@@ -11,14 +9,14 @@ class Solution:
 
         for i in range(n):
 
-            left_sum[i] = tot_till
+            left_sum = tot_till
             tot_till += nums[i]
-            right_sum[i] = tot - tot_till
+            right_sum = tot - tot_till
 
             if nums[i] == 0:
-                if abs(left_sum[i] - right_sum[i]) == 1:
+                if abs(left_sum - right_sum) == 1:
                     res += 1
-                elif left_sum[i] - right_sum[i] == 0:
+                elif left_sum - right_sum == 0:
                     res += 2            
 
         return res
