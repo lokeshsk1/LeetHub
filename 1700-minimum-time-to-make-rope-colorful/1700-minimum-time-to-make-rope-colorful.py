f4cb712057e9b7afb535a378fa9f93cd41f79828
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2025-11-03 23:55:29 +0530

    Time: 184 ms (12.5%), Space: 26.5 MB (35.51%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
@@ -11,12 +11,12 @@ class Solution:
                 tot_time += neededTime[i-1]
                 max_time = max(max_time, neededTime[i-1])
                 i += 1
-            if tot_time > 0:
-                tot_time += neededTime[i-1]
-                max_time = max(max_time, neededTime[i-1])
+                
+            tot_time += neededTime[i-1]
+            max_time = max(max_time, neededTime[i-1])
 
-                res += (tot_time - max_time)
-                tot_time = max_time = 0
+            res += (tot_time - max_time)
+            tot_time = max_time = 0
             
             i += 1
         
