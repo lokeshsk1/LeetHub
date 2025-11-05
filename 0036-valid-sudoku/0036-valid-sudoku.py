21cf8ea3c63d8f62c5d44ae71db400debe165578
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2025-11-05 23:31:33 +0530

    Time: 8 ms (16.24%), Space: 18.1 MB (5.55%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,56 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        def checkRow(i):
+            s = ""
+            for j in range(len(board[i])):
+                if board[i][j] not in ".123456789":
+                    return False
+                if board[i][j] != ".":
+                    s += board[i][j]
+
+            if len(list(s))!=len(set(list(s))):
+                return False
+
+            return True
+
+        def checkCol(j):
+            s = ""
+            for i in range(len(board)):
+                if board[i][j] not in ".123456789":
+                    return False
+                if board[i][j] != ".":
+                    s += board[i][j]
+
+            if len(list(s))!=len(set(list(s))):
+                return False
+
+            return True
+
+        def checkBox(i):
+            u = i
+            r = (i//3) * 3 #3 for 3to5
+            c = (i%3) * 3 # 0,3,6 -> 0
+
+            s = ""
+            for i in range(r, r+3):
+                for j in range(c, c+3):
+                    if board[i][j] not in ".123456789":
+                        return False
+                    if board[i][j] != ".":
+                        s += board[i][j]
+
+            if len(list(s)) != len(set(list(s))):
+                return False
+                
+            return True
+
+        res = True
+        for i in range(9):
+            if not (checkRow(i) and checkCol(i) and checkBox(i)):
+                return False
+        return True
+        
+        
+
+        
