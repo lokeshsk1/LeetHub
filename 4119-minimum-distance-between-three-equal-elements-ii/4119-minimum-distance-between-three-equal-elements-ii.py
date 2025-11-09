5132d5dbd15f902875b458a34eb9f66e32eb9ecb
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2025-11-09 13:07:01 +0530

    Time: 615 ms (100%), Space: 48.2 MB (100%) - LeetHub

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/4119-minimum-distance-between-three-equal-elements-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+
+        if len(nums) < 3:
+            return -1
+
+        res = float('inf')
+        hmap = defaultdict(list)
+
+        for i in range(len(nums)):
+            hmap[nums[i]].append(i)
+        
+        for i,j in hmap.items():
+            if len(j) >= 3:
+                print(j)
+                j.sort()
+                for k in range(len(j)-2):
+                    res = min(res, 2*(max(j[k:k+3]) - min(j[k:k+3])))
+
+        # print(hmap)
+        return -1 if res==float(inf) else res
+
