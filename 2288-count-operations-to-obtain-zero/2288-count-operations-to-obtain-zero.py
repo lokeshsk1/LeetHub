c8107abaad3ce89cc3b049ec755c6fdd25015164
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2025-11-09 14:19:01 +0530

    Time: 0 ms (100%), Space: 18 MB (13.37%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -5,9 +5,10 @@ class Solution:
 
         while num1 and num2:
             if num1 > num2:
-                num1 -= num2
+                res += (num1 // num2)
+                num1 %= num2
             else:
-                num2 -= num1
-            res += 1
+                res += (num2 // num1)
+                num2 %= num1
         
         return res
