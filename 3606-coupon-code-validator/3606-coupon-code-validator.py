94ac8e0e1ba4a1de0f0a36dd09717924bbfffaf0
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2025-12-13 12:12:48 +0530

    Time: 15 ms (27.33%), Space: 18 MB (81.99%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.py
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -5,10 +5,6 @@ class Solution:
             if len(code[i])>0 and all(c.isalnum() or c == '_' for c in code[i]) and businessLine[i] in ("electronics", "grocery", "pharmacy", "restaurant") and isActive[i]:
                 res.append([code[i], businessLine[i]])
         
-        print(res)
-        res.sort(key = lambda x : x[1])
         res.sort(key = lambda x : x[0])
-        res1 = []
-        for i in res:
-            res1.append(i[0])
-        return res1
+        res.sort(key = lambda x : x[1])
+        return [i[0] for i in res]
