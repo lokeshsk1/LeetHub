ca1fa35f61c8b60a4f47fdde1aa8036ff7f64815
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-04 20:23:33 +0530

    Time: 303 ms (18.82%), Space: 25 MB (96.76%) - LeetHub

--- 3799-word-squares-ii/3799-word-squares-ii.py
+++ 3799-word-squares-ii/3799-word-squares-ii.py
@@ -10,13 +10,9 @@ class Solution:
                     for l in range(n):
                         if words[i][0] == words[j][0] and words[i][-1] == words[k][0]:
                             if words[l][0] == words[j][-1] and words[l][-1] == words[k][-1]:
-                                resl = set()
-                                resl.add(words[i])
-                                resl.add(words[j])
-                                resl.add(words[k])
-                                resl.add(words[l])
-                                if len(resl) == 4:
-                                    res.append([words[i], words[j], words[k], words[l]])
+                                square = (words[i], words[j], words[k], words[l])
+                                if len(set(square)) == 4:
+                                    res.append(square)
 
         res.sort()
         return res
