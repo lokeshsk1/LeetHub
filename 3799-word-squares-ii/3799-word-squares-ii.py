29d56c90e76ebe1594da17f126401df23afa5712
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-04 20:25:12 +0530

    Time: 304 ms (18.54%), Space: 26.8 MB (10.34%) - LeetHub

--- 3799-word-squares-ii/3799-word-squares-ii.py
+++ 3799-word-squares-ii/3799-word-squares-ii.py
@@ -10,7 +10,7 @@ class Solution:
                     for l in range(n):
                         if words[i][0] == words[j][0] and words[i][-1] == words[k][0]:
                             if words[l][0] == words[j][-1] and words[l][-1] == words[k][-1]:
-                                square = (words[i], words[j], words[k], words[l])
+                                square = [words[i], words[j], words[k], words[l]]
                                 if len(set(square)) == 4:
                                     res.append(square)
 
