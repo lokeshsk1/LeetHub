c5dbac44d789947afeb0e52f101fae0022233e11
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-13 12:54:14 +0530

    Time: 3 ms (12.26%), Space: 19.5 MB (6.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -13,11 +13,11 @@ class Solution:
             if nums[i] == 0:
                 nums[i], nums[l] = nums[l], nums[i]
                 l += 1
+                i += 1
             elif nums[i] == 2:
                 nums[i], nums[r] = nums[r], nums[i]
                 r -= 1
-                i -= 1
-            
-            i += 1
+            else:
+                i += 1
 
             print(nums)
