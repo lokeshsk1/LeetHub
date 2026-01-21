66c9b80bd8651de2fdcde18732fc739d221da2cf
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-22 01:25:05 +0530

    Time: 1450 ms (78.26%), Space: 19.5 MB (32.98%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,24 +1,36 @@
 class Solution:
     def numSquares(self, n: int) -> int:
 
-        self.memo = [-1] * (n+1)
-        return self.recurse(n)
+        dp = [n+1] * (n+1)
+        
+        for i in range(0, n+1):
+            if i < 4:
+                dp[i] = i
+            else:
+                for j in range(1, int(i**0.5) + 1):
+                    dp[i] = min(dp[i], 1 + dp[i - (j*j)])
+
+        print(dp)
+        return dp[n]
+
+    #     self.memo = [-1] * (n+1)
+    #     return self.recurse(n)
 
     
-    def recurse(self, n):
+    # def recurse(self, n):
 
-        if self.memo[n] != -1:
-            return self.memo[n]
+    #     if self.memo[n] != -1:
+    #         return self.memo[n]
 
-        if n < 4:
-            self.memo[n] = n
-            return n
+    #     if n < 4:
+    #         self.memo[n] = n
+    #         return n
         
-        mini = n
+    #     mini = n
 
-        for i in range(1, int(n**0.5) + 1):
-            mini = min(mini, 1 + self.recurse(n - (i*i)))
+    #     for i in range(1, int(n**0.5) + 1):
+    #         mini = min(mini, 1 + self.recurse(n - (i*i)))
         
-        self.memo[n] = mini
-        return mini
+    #     self.memo[n] = mini
+    #     return mini
 
