906497ed3ba0136c59a1bdbafa0662cd5051c2fc
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-24 20:51:44 +0530

    Time: 52 ms (37.78%), Space: 19.2 MB (19.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,4 +1,6 @@
 class Solution:
+
+    #Recursion
     # def fib(self, n: int) -> int:
 
     #     if n < 2:
@@ -6,20 +8,32 @@ class Solution:
         
     #     return self.fib(n-2) + self.fib(n-1)
 
-    def fib(self, n: int) -> int:
-        self.memo = [-1] * (n+1)
-        return self.recurse(n)
+    #Recursion + Memoization
+    # def fib(self, n: int) -> int:
+    #     self.memo = [-1] * (n+1)
+    #     return self.recurse(n)
     
-    def recurse(self, n):
+    # def recurse(self, n):
 
-        if n < 2:
-            return n
+    #     if n < 2:
+    #         return n
         
-        if self.memo[n] != -1:
-            return self.memo[n]
+    #     if self.memo[n] != -1:
+    #         return self.memo[n]
         
-        self.memo[n] = self.recurse(n-2) + self.recurse(n-1)
-        return self.memo[n]
+    #     self.memo[n] = self.recurse(n-2) + self.recurse(n-1)
+    #     return self.memo[n]
+
+    #DP
+    def fib(self, n: int) -> int:
+
+        dp = [0,1]
+
+        for i in range(2, n+1):
+            dp.append(dp[i-2] + dp[i-1])
+        
+        return dp[n]
+
 
         
         
