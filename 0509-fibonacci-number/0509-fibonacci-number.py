280041f59a35e9be729f2f191a2fc13378ee3fc0
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-24 20:46:32 +0530

    Time: 45 ms (48.81%), Space: 19.3 MB (6.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,26 @@
 class Solution:
+    # def fib(self, n: int) -> int:
+
+    #     if n < 2:
+    #         return n
+        
+    #     return self.fib(n-2) + self.fib(n-1)
+
     def fib(self, n: int) -> int:
+        self.memo = [-1] * (n+1)
+        return self.recurse(n)
+    
+    def recurse(self, n):
 
         if n < 2:
             return n
         
-        return self.fib(n-2) + self.fib(n-1)
+        if self.memo[n] != -1:
+            return self.memo[n]
+        
+        self.memo[n] = self.recurse(n-2) + self.recurse(n-1)
+        return self.memo[n]
+
+        
+        
+
