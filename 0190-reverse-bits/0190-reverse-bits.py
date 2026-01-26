9b84a4f1da6b6e721b5672ffccbdcf356a17276c
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-26 17:50:19 +0530

    Time: 48 ms (19.38%), Space: 19.4 MB (11.69%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -4,10 +4,10 @@ class Solution:
         res = 0
         
         for i in range(32):
-            res <<= 1
             res += n & 1
             n >>= 1
+            res <<= 1
         
-        return res
+        return res >> 1
 
 
