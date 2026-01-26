6b4098d147ba2b5cb63ad4b0cdcd98c0c47f7988
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-01-26 23:49:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (33.01%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -3,5 +3,5 @@ class Solution:
 
         for i in range(1, n):
             j = n - i
-            if '0' not in str(i) and '0' not in str(j):
+            if '0' not in str(i) + str(j):
                 return [i, j]
