c5825d0bf6ad25edc0e4da6e81b3127ecca91d99
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-02-22 17:54:13 +0530

    Time: 4 ms (56.2%), Space: 19.4 MB (84.75%) - LeetHub

--- 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
@@ -1,8 +1,5 @@
 class Solution:
     def isDigitorialPermutation(self, n: int) -> bool:
 
-        factSum = 0
-        for i in str(n):
-            factSum += math.factorial(int(i))
-
+        factSum = sum(math.factorial(int(i)) for i in str(n))
         return Counter(str(factSum)) == Counter(str(n))
