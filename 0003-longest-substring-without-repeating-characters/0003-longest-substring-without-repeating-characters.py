e1380dad676accffb5eb30b29da5983b6c13abe3
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-03-16 22:25:53 +0530

    Time: 12 ms (56.57%), Space: 19.2 MB (87.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,8 +8,7 @@ class Solution:
         for i,e in enumerate(s):
             if e in vis:
                 last = max(last, vis[e] + 1)
-            vis[e] = i
-
             res = max(res, i - last + 1)
+            vis[e] = i
 
         return res
