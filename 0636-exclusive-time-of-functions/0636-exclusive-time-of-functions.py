ed61f8170d266537aa44628bbe0d7d74ad72eb91
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-15 01:58:43 +0530

    Time: 3 ms (94.87%), Space: 19.3 MB (88.33%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -0,0 +1,26 @@
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        
+        res = [0]*n
+
+        st = []
+
+        for l in logs:
+            idn, status, time = l.split(":")
+            idn, time = int(idn), int(time)
+            
+            if status == "start":
+                st.append((idn, time))
+            
+            if status == "end":
+                start_idn , start_time = st.pop()
+                diff_time = time - start_time + 1
+                res[idn] += diff_time
+
+                if st:
+                    start_idn1 , start_time1 = st[-1]
+                    res[start_idn1] -= diff_time
+        
+        return res
+
+
