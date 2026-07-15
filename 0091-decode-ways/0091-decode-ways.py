1b441a2561215522b1dfc6ba26528311f119722e
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-16 01:27:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (70.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,16 +4,16 @@ class Solution:
         if s[0] == '0':
             return 0
 
-        dp = [0]*(len(s)+1)
+        dp = [0] * (len(s)+1)
         dp[0] = 1
         dp[1] = 1 if s[0] != '0' else 0
 
         for i in range(2, len(s)+1):
 
-            if int(s[i-2] + s[i-1]) in range(10,27):
-                dp[i] += dp[i-2]
- 
             if s[i-1] != '0':
-                dp[i] += dp[i-1]
+                dp[i] = dp[i-1]
+            
+            if int(s[i-2:i]) in range(10, 27):
+                dp[i] += dp[i-2]
         
         return dp[-1]
