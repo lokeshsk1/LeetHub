7c54dfcb4e6eeb13bf369cd1d751e453e88e4018
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-20 00:14:24 +0530

    Time: 2151 ms (24.51%), Space: 19.1 MB (93.32%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,32 +1,20 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-
-        dp = [i for i in range(n+1)]
         
-        for i in range(0, n+1):
-            for j in range(1, int(i**0.5) + 1):
-                dp[i] = min(dp[i], 1 + dp[i - (j*j)])
+        ps = []
+
+        i = 1
+        while i*i <= n:
+            ps.append(i*i)
+            i += 1
+        
+        ps = set(ps)
+
+        dp = [n+1] * (n+1)
+        dp[0] = 0
+
+        for i in range(1, n+1):
+            for j in ps:
+                dp[i] = min(dp[i], dp[i-j]+1)
 
         return dp[n]
-
-    #     self.memo = [-1] * (n+1)
-    #     return self.recurse(n)
-
-    
-    # def recurse(self, n):
-
-    #     if self.memo[n] != -1:
-    #         return self.memo[n]
-
-    #     if n < 4:
-    #         self.memo[n] = n
-    #         return n
-        
-    #     mini = n
-
-    #     for i in range(1, int(n**0.5) + 1):
-    #         mini = min(mini, 1 + self.recurse(n - (i*i)))
-        
-    #     self.memo[n] = mini
-    #     return mini
-
