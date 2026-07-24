9aed90acfcab38ec742634bec747401c337c6108
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-24 18:52:07 +0530

    Time: 648 ms (77.95%), Space: 19.5 MB (69.2%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -0,0 +1,22 @@
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        
+        n = len(scores)
+
+        scores = [score for _, score in sorted(zip(ages, scores), reverse = 1) ]
+        ages = sorted(ages, reverse = 1)
+
+        dp = scores[:]
+        res = 0
+
+        # print(scores)
+
+        for i in range(n):
+            for j in range(i):
+                if scores[j] >= scores[i]:
+                    dp[i] = max(dp[i], dp[j] + scores[i])
+            
+            res = max(res, dp[i])
+            # print(dp)
+        
+        return res
