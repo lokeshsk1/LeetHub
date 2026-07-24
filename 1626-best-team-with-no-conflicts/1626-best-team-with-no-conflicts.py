68a82b165c80e4114ce23e67b0ed104136452f29
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-25 01:09:46 +0530

    Time: 645 ms (78.65%), Space: 19.6 MB (39.33%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -3,8 +3,11 @@ class Solution:
         
         n = len(scores)
 
-        scores = [score for _, score in sorted(zip(ages, scores), reverse = 1) ]
-        ages = sorted(ages, reverse = 1)
+        # scores = [score for _, score in sorted(zip(ages, scores), reverse = 1) ]
+        # ages = sorted(ages, reverse = 1)
+
+        scores = [score for _, score in sorted(zip(ages, scores), reverse = 0) ]
+        ages = sorted(ages, reverse = 0)
 
         dp = scores[:]
         res = 0
@@ -13,10 +16,11 @@ class Solution:
 
         for i in range(n):
             for j in range(i):
-                if scores[j] >= scores[i]:
+                if scores[j] <= scores[i]:
                     dp[i] = max(dp[i], dp[j] + scores[i])
             
             res = max(res, dp[i])
+
             # print(dp)
-        
+
         return res
