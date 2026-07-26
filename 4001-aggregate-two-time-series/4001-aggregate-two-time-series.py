8313be4f26938ff0909423392398f16c586354c3
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-26 19:28:13 +0530

    Time: 145 ms (65.54%), Space: 67.9 MB (67.94%) - LeetHub

---
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.py
@@ -0,0 +1,28 @@
+class Solution:
+    def aggregateTimeSeries(self, series1: list[list[int]], series2: list[list[int]]) -> list[list[int]]:
+
+        p1 = 0
+        p2 = 0
+
+        res = []
+
+        while p1 < len(series1) and p2 < len(series2):
+            
+            Sum = series1[p1][1] + series2[p2][1]
+            
+            time = min(series1[p1][0] , series2[p2][0])
+            
+            res.append([time, Sum])
+
+            if series1[p1][0] == time:
+                p1 += 1
+            if series2[p2][0] == time:
+                p2 += 1
+
+        if p1 >= len(series1):
+            res += series2[p2:]
+            
+        if p2 >= len(series2):
+            res += series1[p1:]
+
+        return res
