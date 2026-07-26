d37f731743787fa636ede7383417e04335caf837
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-26 19:29:34 +0530

    Time: 139 ms (70.26%), Space: 67.9 MB (54.18%) - LeetHub

--- 4001-aggregate-two-time-series/4001-aggregate-two-time-series.py
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.py
@@ -1,18 +1,16 @@
 class Solution:
     def aggregateTimeSeries(self, series1: list[list[int]], series2: list[list[int]]) -> list[list[int]]:
 
-        p1 = 0
-        p2 = 0
-
+        p1 = 0 ; p2 = 0
         res = []
 
         while p1 < len(series1) and p2 < len(series2):
             
-            Sum = series1[p1][1] + series2[p2][1]
+            tot = series1[p1][1] + series2[p2][1]
             
             time = min(series1[p1][0] , series2[p2][0])
             
-            res.append([time, Sum])
+            res.append([time, tot])
 
             if series1[p1][0] == time:
                 p1 += 1
