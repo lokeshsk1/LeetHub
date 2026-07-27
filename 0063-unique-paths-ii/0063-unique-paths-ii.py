689320b75eba5b1078e1406c4ab6903d0f2d079f
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-27 18:16:19 +0530

    Time: 4 ms (7.74%), Space: 19.2 MB (98.23%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,26 +1,41 @@
 class Solution:
-    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
 
-        if grid[0][0]==1 or grid[-1][-1]==1:
-            return 0
+        dp = obstacleGrid
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
 
-        r = len(grid); c = len(grid[0])
+        # dp = [[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if dp[i][j] == 1:
+                    dp[i][j] = 'X'
+                    if i == 0:
+                        for k in range(j, n):
+                            dp[0][k] = 'X'
+                    if j == 0:
+                        for k in range(i, m):
+                            dp[k][0] = 'X'
+
+        print(dp)
+
+        for i in range(m):
+            for j in range(n):
+                
+                if dp[i][j] == 'X':
+                    continue
+                elif i == 0 or j == 0:
+                    dp[i][j] = 1
+                elif dp[i-1][j] == 'X':
+                    dp[i][j] = dp[i][j-1]
+                elif dp[i][j-1] == 'X':
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            
+        print(dp)
         
-        for i in range(r):
-            for j in range(c):
-                if grid[i][j] == 1:
-                    grid[i][j] = -1
+        return 0 if dp[-1][-1] == 'X' else dp[-1][-1]
 
-        grid[0][0] = 1
-
-        for i in range(r):
-            for j in range(c):
-                if grid[i][j] != -1:
-                    if i > 0 and grid[i-1][j]!=-1:
-                        grid[i][j] += grid[i-1][j]
-                    if j > 0 and grid[i][j-1]!=-1:
-                        grid[i][j] += grid[i][j-1]
         
-
-        return grid[-1][-1]
         
