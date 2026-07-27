8e4bd1293f6234a472d61743a926df34b7880b9b
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-27 21:02:29 +0530

    Time: 393 ms (41.28%), Space: 44.4 MB (44.74%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,19 +6,13 @@ class Solution:
 
         dp = [[0] * (n2+1) for _ in range(n1+1)]
 
-        print(dp)
-
-        for i in range(n1+1):
-            for j in range(n2+1):
-                if i == 0 or j == 0:
-                    dp[0][0] = 0
-                elif s1[i-1] == s2[j-1]:
+        for i in range(1, n1+1):
+            for j in range(1, n2+1):
+                
+                if s1[i-1] == s2[j-1]:
                     dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
                 
-                
-        # print(dp)
-
         return dp[-1][-1]
         
