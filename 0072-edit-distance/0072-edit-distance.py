c7acf9c3f33102e70f06cd1d2ce2ed343a6bd671
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-28 02:29:07 +0530

    Time: 71 ms (7.69%), Space: 26.3 MB (7.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,24 +1,52 @@
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
+    def minDistance(self, s1: str, s2: str) -> int:
         
-        n1 = len(word1); n2 = len(word2)
 
+        # #delete
+        # dance
+        # danc
+        # ignore last char in s1 + 1
+
+        # #replace
+        # dancer
+        # dancex
+        # ignore last char in both + 1
+
+        # #create
+        # dance
+        # dancer
+        # ignore last char in s2 + 1
+
+
+        n1 = len(s1); n2 = len(s2)
         dp = [[0]*(n2+1) for _ in range(n1+1)]
 
-        print(dp)
+        for i in range(n2+1):
+            dp[0][i] = i
+        for j in range(n1+1):
+            dp[j][0] = j
 
-        for i in range(n1+1):
-            dp[i][0] = i
-        for j in range(n2+1):
-            dp[0][j] = j
+        print(dp)
 
         for i in range(1, n1+1):
             for j in range(1, n2+1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                else:
+                if s1[i-1] != s2[j-1]:
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-
+                else:
+                    dp[i][j] = dp[i-1][j-1]
+        
         print(dp)
 
         return dp[-1][-1]
+
+        # abc
+        # xyz
+
+        #        a b c
+        #      0 1 2 3
+        #   0. 0 1 2 3
+        # x 1. 1 1 2 3
+        # y 2. 2 2 2 3
+        # z 3. 3 3 3 3
+
+
