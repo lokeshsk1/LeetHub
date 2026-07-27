0d2d8f8ca1380322c5750de52b0579c137da1623
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-27 18:27:43 +0530

    Time: 3 ms (21.08%), Space: 19.2 MB (88.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -10,29 +10,19 @@ class Solution:
             for j in range(n):
                 if dp[i][j] == 1:
                     dp[i][j] = 'X'
-                    if i == 0:
-                        for k in range(j, n):
-                            dp[0][k] = 'X'
-                    if j == 0:
-                        for k in range(i, m):
-                            dp[k][0] = 'X'
 
-        print(dp)
+        if dp[0][0] != 'X':
+            dp[0][0] = 1 
 
         for i in range(m):
             for j in range(n):
-                
                 if dp[i][j] == 'X':
                     continue
-                elif i == 0 or j == 0:
-                    dp[i][j] = 1
-                elif dp[i-1][j] == 'X':
-                    dp[i][j] = dp[i][j-1]
-                elif dp[i][j-1] == 'X':
+                if i>0 and dp[i-1][j] != 'X':
                     dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
-            
+                if j > 0 and dp[i][j-1] != 'X':
+                    dp[i][j] += dp[i][j-1]
+                
         print(dp)
         
         return 0 if dp[-1][-1] == 'X' else dp[-1][-1]
