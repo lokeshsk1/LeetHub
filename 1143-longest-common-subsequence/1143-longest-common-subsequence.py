e666569513f86ab7710d2757c52f421edc45aee2
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-27 21:01:17 +0530

    Time: 494 ms (30.39%), Space: 47.3 MB (19.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,24 +1,24 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
-        n1 = len(text1)
-        n2 = len(text2)
+    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
 
-        dp = [[0]*(n2+1) for _ in range(n1+1)]
+        n1 = len(s1)
+        n2 = len(s2)
 
-        for i in range(1, n1+1):
-            for j in range(1, n2+1):
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
+        dp = [[0] * (n2+1) for _ in range(n1+1)]
+
+        print(dp)
+
+        for i in range(n1+1):
+            for j in range(n2+1):
+                if i == 0 or j == 0:
+                    dp[0][0] = 0
+                elif s1[i-1] == s2[j-1]:
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
                 else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                
+                
+        # print(dp)
 
         return dp[-1][-1]
-
-        #    "" a b c d e
-        # ""  0 0 0 0 0 0
-        # a.  0 1 
-        # b.  0
-        # c.  0
-        # d.  0
-        # e.  0
+        
