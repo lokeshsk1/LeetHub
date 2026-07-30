f446a14cb9b78ca5a748eeb5a6155bbd5a81fc45
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-30 23:32:42 +0530

    Time: 11 ms (32.96%), Space: 21.2 MB (48.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -9,9 +9,10 @@ class Solution:
             if nums[i] == curr:
                 count += 1
             else:
-                if count == 1:
+                count -= 1
+                if count == 0:
                     curr = nums[i]
-                else:
-                    count -= 1
+                    count = 1
+                    
         
         return curr
