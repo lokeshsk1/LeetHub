288bc1b605db48ce81479cd4d78d3cbfde225ac1
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-07-31 00:14:38 +0530

    Time: 3 ms (10.71%), Space: 19.4 MB (21.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,19 +4,26 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        l,m = 0,0
+        l = 0
+        i = 0
+        r = len(nums)-1
 
-        for i in nums:
-            if i == 0:
+        while i <= r:
+            if nums[i] == 0:
+                nums[l], nums[i] = nums[i], nums[l]
                 l += 1
-                m += 1
-            elif i == 1:
-                m += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[r], nums[i] = nums[i], nums[r]
+                r -= 1
+            else:
+                i += 1
+            
+            
+            print(nums)
         
         # print(l,m)
 
-        nums[:l] = [0]*l
-        nums[l:m] = [1]*(m-l)
-        nums[m:] = [2]*(len(nums)-m)
-
-        return nums
+        # nums[:l] = [0]*l
+        # nums[l:m] = [1]*(m-l)
+        # nums[m:] = [2]*(len(nums)-m)
