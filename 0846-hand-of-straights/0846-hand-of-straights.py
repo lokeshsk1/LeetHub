ab020b3bc047bc49ca3844cbb48330efe1b0185e
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-08-05 01:17:52 +0530

    Time: 31 ms (67.35%), Space: 21.1 MB (61.2%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,16 +1,15 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], g: int) -> bool:
 
-        while hand:
+        c = Counter(hand)
 
-            mn = min(hand)
-
-            for j in range(mn, mn+g):
-                try:
-                    hand.remove(j)
-                    print(j)
-                except:
-                    return False
+        for i in sorted(c):
+            if c[i] > 0:
+                curr = c[i]
+                for j in range(g):
+                    c[i+j] -= curr
+                    if c[i+j] < 0:
+                        return False
         
         return True
             
