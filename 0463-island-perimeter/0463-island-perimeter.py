af6c840be2502bcacfff178239466ee531f1e28f
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-08-12 00:07:27 +0530

    Time: 89 ms (41.27%), Space: 20 MB (39.45%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -5,30 +5,20 @@ class Solution:
         self.r = len(grid)
         self.c = len(grid[0])
 
+        count = 0
+
         for i in range(self.r):
             for j in range(self.c):
                 if grid[i][j] == 1:
-                    self.dfs(grid,i,j)
+                    count += 4
+                    if i > 0 and grid[i-1][j] == 1:
+                        count -= 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        count -= 2
+                print(count)
         
-        return self.res
+        return count
+                    
 
-
-    def dfs(self, grid, i, j):
         
-        #top
-        if i == 0 or (i-1 >= 0 and grid[i-1][j] == 0):
-            self.res += 1
-
-        #bottom
-        if i == self.r-1 or (i+1 <= self.r and grid[i+1][j] == 0):
-            self.res += 1
-
-        #left
-        if j == 0 or (j-1 >= 0 and grid[i][j-1] == 0):
-            self.res += 1
         
-        #right
-        if j == self.c-1 or (j+1 >= 0 and grid[i][j+1] == 0):
-            self.res += 1
-            
-
