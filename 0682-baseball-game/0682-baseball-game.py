1a74551b1f21e190c5377412ed8a298d1e9e2ac3
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-08-31 17:40:06 +0530

    Time: 62 ms (3.43%), Space: 19.8 MB (12.71%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -4,13 +4,15 @@ class Solution:
         st = []
 
         for i in ops:
-            try:
+            # try:
+            #     st.append(int(i))
+            #     continue
+            # except:
+            #     pass
+
+            if i.isnumeric() or i[0]=="-":
                 st.append(int(i))
-                continue
-            except:
-                pass
-            
-            if i == '+':
+            elif i == '+':
                 st.append(st[-1] + st[-2])
             elif i == 'D':
                 st.append(st[-1] * 2)
