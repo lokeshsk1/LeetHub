ff59de3917d0e14a85d701bf06d299a8928c5ab7
Lokesh Senthilkumar <66515225+lokeshsk1@users.noreply.github.com>
2026-09-07 21:07:16 +0530

    Time: 37 ms (31.32%), Space: 22.3 MB (14.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,16 +3,16 @@ class Solution:
         
         res = 0
         prefix = 0
-        dt = {0:1}
-        
-        
+        dt = defaultdict(int)
+        dt[0] = 1
+
         for i in nums:
             prefix += i
             
             if prefix - k in dt:
                 res += dt[prefix - k]
             
-            dt[prefix] = dt.get(prefix, 0) + 1
+            dt[prefix] = dt[prefix] + 1
             
         
         return res
